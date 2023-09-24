23bba96de00cd386b4aed1e2152b78e7cf98f1e1
Emimint <90863470+Emimint@users.noreply.github.com>
2023-09-24 16:57:11 -0400

    ajoute menu bouton pour deconnexion
    Le button du prenom donne acces au bouton de deconnexion

---
+++ Project/src/main/java/com/example/project/controllers/Location.java
@@ -0,0 +1,6 @@
+package com.example.project.controllers;
+
+public class Location {
+    // Classe manipulee des objets de type Location qui seront inseres dans la base de donnees
+    // ////////////////////////////////////// //
+}
