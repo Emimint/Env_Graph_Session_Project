10decafda03529576d39c05ebc0a5ebec4788052
Emimint <90863470+Emimint@users.noreply.github.com>
2023-09-24 20:58:35 -0400

    cree classe LoginController.java

---
+++ Project/src/main/java/com/example/project/controllers/LocationController.java
@@ -0,0 +1,21 @@
+package com.example.project.controllers;/*
+ * @created 24/09/2023 - 5:11 p.m.
+ * @project Env_Graph_Session_Project
+ * @author Emilie Echevin
+ */
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class LocationController implements EventHandler<ActionEvent> {
+
+    private Location model;
+    private
+
+    public LocationController
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+
+    }
+}
