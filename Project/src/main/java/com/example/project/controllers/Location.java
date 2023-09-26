9c2d859843425090eb404fd684423b61159c49ef
Emimint <90863470+Emimint@users.noreply.github.com>
2023-09-26 12:41:29 -0400

    met a jour mutateurs et constructeurs

--- Project/src/main/java/com/example/project/controllers/Location.java
+++ Project/src/main/java/com/example/project/controllers/Location.java
@@ -8,7 +8,7 @@ public class Location {
     private int  superficie;
     private int  annee_construction; // doit varier de 1900 a l'annee courante (2023)
 
-//    public Location(){}
+    public Location(){}
 
     public Location(int ID, String no_local, String adresse, int superficie, int annee_construction){
         this.ID = ID;
@@ -19,24 +19,40 @@ public class Location {
     }
 
     // Mutateurs (ils vont generer des messages d'erreur qui seront utilises pour avertir l'utilisateur via des boites d'alerte) :
+    public void setID(int ID){
+        this.ID = ID;
+    }
+
     public void setNoLocal(String no_local){
-        if(no_local == "") throw new IllegalArgumentException("Vous devez specifie un numero de local.");
+        if(no_local.isEmpty()) throw new IllegalArgumentException("Vous devez specifie un numero de local.");
         this.no_local = no_local;
     }
 
     public void setAdresse(String adresse){
-        if(adresse == "") throw new IllegalArgumentException("Vous devez specifier une adresse.");
+        if(adresse.isEmpty()) throw new IllegalArgumentException("Vous devez specifier une adresse.");
         this.adresse = adresse;
     }
 
-    public void setSuperficie(int superficie){
-        if(superficie < 10) throw new IllegalArgumentException("La superficie doit etre d'au moins 10 metres carre.");
-        this.superficie = superficie;
+    public void setSuperficie(String inputSuperficie) {
+        try {
+            int superficie = Integer.parseInt(inputSuperficie);
+            if (superficie < 10) {
+                throw new IllegalArgumentException("La superficie doit être d'au moins 10 mètres carrés.");
+            }
+            this.superficie = superficie;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("La valeur de la superficie doit être un nombre entier.");
+        }
     }
 
-    public void setAnneeConstruction(int annee_construction){
-        if(annee_construction < 1990 || annee_construction > 2023) throw new IllegalArgumentException("L'annee de construction doit varier de 1990 a 2023.");
-        this.annee_construction = annee_construction;
+    public void setAnneeConstruction(String inputAnneeConstruction) {
+        try {
+            int annee_construction = Integer.parseInt(inputAnneeConstruction);
+            if(annee_construction < 1990 || annee_construction > 2023) throw new IllegalArgumentException("L'annee de construction doit varier de 1990 a 2023.");
+            this.annee_construction = annee_construction;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("L'annee de construction doit être comprise entre 1990 et 2023.");
+        }
     }
 
     // Accesseurs (ils doivent avec exactement la bonne syntaxe - get+nom de la variable - pour etre reconnus par la methode setCellValueFactory) :
